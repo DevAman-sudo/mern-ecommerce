2bd2af9164bb6b526f1663313113429fab90706e
DevAman-sudo <devaman.net@gmail.com>
2022-06-21 10:34:08 +0545

    devil

---
+++ frontend/src/component/layout/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div></div>
+    // <div className="loading">
+    //   <div></div>
+    // </div>
+  );
+};
+
+export default Loader;
